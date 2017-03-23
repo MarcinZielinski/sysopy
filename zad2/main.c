ca5abe6243a1e9bf5ddd786b5ddcbb9c16df60b3
MarcinZielinski <zielinskimarcin123@gmail.com>
2017-03-23 15:36:01 +0100

    Second task - added child memory and cpu time limits. Displaying time the process took

--- zad2/main.c
+++ zad2/main.c
@@ -1,3 +1,7 @@
+//
+// Created by Mrz355 on 23.03.17.
+//
+
 #define _XOPEN_SOURCE 1000
 #include <ftw.h>
 #include <stdio.h>
@@ -6,7 +10,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <wait.h>
-
+#include <sys/resource.h>
+#include <errno.h>
 
 extern char ** environ;
 
@@ -28,7 +33,7 @@ int fn(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf) 
 char* nftwSearch(char* dirPath) {
     int result = nftw(dirPath,fn,50,FTW_PHYS);
     if (result == -1) {
-        perror("Nftw non successful");
+        perror("Nftw not successful");
         exit(EXIT_FAILURE);
     } else if (result == -15) {
         return currentPath;
@@ -70,7 +75,7 @@ char *searchFile(char *programName) {
                 return absPath;
             }
         }
-        printf("Couldn't find file: %s",programName);
+        printf(stderr,"Couldn't find file: %s",programName);
         exit(EXIT_FAILURE);
     }
 
@@ -111,7 +116,18 @@ char **replaceArgumentsWithEnvironmentVariables(char **argv) {
     return argv;
 }
 
-void parseLine(char *line) { // line > 1
+void displayChildUsage(pid_t pid,char* programName) {
+    struct rusage* usage = NULL;
+    if(getrusage(RUSAGE_CHILDREN,usage) == 0) {
+        printf("\nProces PID: %d, program: %s",pid,programName);
+        printf("\nSystem time usage: %zu", usage->ru_stime.tv_sec);
+        printf("\nUser time usage: %zu\n", usage->ru_utime.tv_sec);
+    } else {
+        perror("Couldn't get the usage of child process.");
+    }
+}
+
+void parseLine(char *line, rlim_t virtual_limit, rlim_t cpu_limit) { // line > 1
     char *str;
 
     while(line[0]==' ') { // delete the spaces before command
@@ -147,27 +163,62 @@ void parseLine(char *line) { // line > 1
             argv = replaceArgumentsWithEnvironmentVariables(argv);
             pid_t child = fork();
             if(child == 0) {
+                int result1, result2;
+                struct rlimit *limit;
+                limit->rlim_max = virtual_limit;
+                result1 = setrlimit(RLIMIT_AS,limit);
+                limit->rlim_max = cpu_limit;
+                result2 = setrlimit(RLIMIT_CPU,limit);
+                if(result1 != 0 || result2 != 0) {
+                    perror("Cannot set given limits to the PC resources");
+                    exit(EXIT_FAILURE);
+                }
+                free(limit);
                 execve(programPath, argv, environ);
             }
             int status;
             wait(&status);
             if(status != 0) {
-                printf("\nProgram \"%s\" execution failed\n",programName);
+                printf(stderr,"\nProgram \"%s\" execution failed\n",programName);
                 exit(EXIT_FAILURE);
             }
+
+            displayChildUsage(child,programName);
+
         } else {
-            printf("\nCouldn't find program: %s",programName);
+            printf(stderr,"\nCouldn't find program: %s",programName);
             exit(EXIT_FAILURE);
         }
     }
 }
 
+long stol(char* str) {
+    char *endptr;
+    errno = 0;
+
+    long res = strtol(str,&endptr,10);
+
+    if ((errno == ERANGE && (res == LONG_MAX || res == LONG_MIN))
+        || (errno != 0 && res == 0)) {
+        perror("Cannot convert argument to string\n");
+        exit(EXIT_FAILURE);
+    }
+    if (endptr == str) {
+        fprintf(stderr, "No digits were found\n");
+        exit(EXIT_FAILURE);
+    }
+    return res;
+}
+
 int main(int argc, char **argv) {
-    if(argc!=2) {
-        printf("Not a valid number of arguments! 1 required - Path to file to interpret.\n");
+    if(argc!=4) {
+        printf(stderr,"Not a valid number of arguments! 3 required - Path to file to interpret, maximum virtual memory usage and maximum cpu time usage.\n");
         exit(EXIT_FAILURE);
     }
     char* dirPath = argv[1];
+
+    long virtual_limit = stol(argv[2]);
+    long cpu_limit = stol(argv[3]);
 
     FILE * file1;
     if((file1 = fopen(dirPath,"r")) == NULL) {
@@ -188,7 +239,7 @@ int main(int argc, char **argv) {
 
     while ((read = getline(&line, &len, file1)) != -1) {
         if(read > 1) {  // > 1, because of the endline character
-            parseLine(line);
+            parseLine(line, (rlim_t) virtual_limit, (rlim_t) cpu_limit);
         }
     }
 
